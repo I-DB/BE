f9c56ea966456fa079758671d320635fe7652cd2
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-10 18:17:15 +0900

    dev 안먹힌거 재수정

--- server.js
+++ server.js
@@ -5,41 +5,6 @@ const https = require('https')
 require('dotenv').config()
 const domain = process.env.DOMAIN
 let httpsServer
-// const dev = process.env.DEV
-
-// if (dev === true) {
-// 	const port = 3000
-
-// 	const handleListening = () =>
-// 		console.log(`Listening on : http://localhost:${port}`)
-
-// 	app.listen(port, handleListening())
-// } else if (dev === false) {
-// 	const port = 443
-// 	const privateKey = fs.readFileSync(
-// 		`/etc/letsencrypt/live/${domain}/privkey.pem`,
-// 		'utf8'
-// 	)
-// 	const certificate = fs.readFileSync(
-// 		`/etc/letsencrypt/live/${domain}/cert.pem`,
-// 		'utf8'
-// 	)
-// 	const ca = fs.readFileSync(
-// 		`/etc/letsencrypt/live/${domain}/chain.pem`,
-// 		'utf8'
-// 	)
-
-// 	const credentials = {
-// 		key: privateKey,
-// 		cert: certificate,
-// 		ca: ca,
-// 	}
-
-// 	const httpsServer = https.createServer(credentials, app)
-// 	httpsServer.listen(port, () => {
-// 		console.log('HTTPS Server running on port 443')
-// 	})
-// }
 
 if (process.env.DEV) {
 	const privateKey = fs.readFileSync(
@@ -67,9 +32,9 @@ if (process.env.DEV) {
 
 const httpServer = http.createServer(app)
 
-httpServer.listen(80, () => {
-	console.log(`Listening on : http://localhost:80`)
-	console.log('HTTPS Server running on port 80')
+httpServer.listen(3000, () => {
+	console.log(`Listening on : http://localhost:3000`)
+	console.log('HTTPS Server running on port 3000')
 })
 
 httpsServer.listen(443, () => {
